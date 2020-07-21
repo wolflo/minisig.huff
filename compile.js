a39bddd4fd442286bfbc3c3c0a7a0742ae850d35
wolflo <wardnick@protonmail.com>
2020-07-21 13:39:56 +0400

    Move bytecode size iteration to compile step

--- compile.js
+++ compile.js
@@ -1,34 +1,51 @@
-const path = require('path');
 const fs = require('fs');
+const path = require('path');
 const parser = require('./huff/src/parser');
 
 const modulesPath = path.posix.resolve(__dirname, './src/huff_modules');
 const OUT_PATH = 'out/';
 
-const minisigParsed = parser.parseFile('minisig.huff', modulesPath);
+const msigParsed = parser.parseFile('minisig.huff', modulesPath);
+
+const runtimeShallow = parser.processMacro(
+  'RUNTIME',
+  0,
+  [ '00' ],
+  msigParsed.macros,
+  msigParsed.inputMap,
+  msigParsed.jumptables
+).data.bytecode;
+
+check(lenBytes(runtimeShallow) <= 255, "bad runtime length estimate");
 
 const runtime = parser.processMacro(
   'RUNTIME',
   0,
-  [],
-  minisigParsed.macros,
-  minisigParsed.inputMap,
-  minisigParsed.jumptables
+  [ lenBytes(runtimeShallow).toString() ],
+  msigParsed.macros,
+  msigParsed.inputMap,
+  msigParsed.jumptables
 ).data.bytecode;
 
-const runtimeShallow = parser.processMacro(
-  'RUNTIME__SHALLOW',
-  0,
-  [],
-  minisigParsed.macros,
-  minisigParsed.jumptables,
-).data.bytecode;
-
-if(runtime.length !== runtimeShallow.length) throw "runtimeShallow incorrect length";
+check(lenBytes(runtime) === lenBytes(runtimeShallow), "bad runtime length");
 
 writeBin('minisig-runtime.bin', runtime);
-
 console.log(`bytecode written to ${OUT_PATH}`);
+
+function lenBytes(str) {
+  return trimBytes(str).length / 2
+}
+
+function trimBytes(str) {
+  if (str.length % 2 !== 0) {
+    throw 'ERR: These aint bytes'
+  }
+  return str.replace(/^0x/,'')
+}
+
+function check(pred, err) {
+  if (!pred) throw err;
+}
 
 function writeBin(filename, bytecode) {
   fs.writeFileSync(path.posix.resolve(__dirname, OUT_PATH + filename), bytecode);
