e52a76aaaa27851fb8a913c67412c3ef6487cf22
wolflo <wardnick@protonmail.com>
2020-07-21 13:44:19 +0400

    Add constructor

--- compile.js
+++ compile.js
@@ -6,6 +6,7 @@ const modulesPath = path.posix.resolve(__dirname, './src/huff_modules');
 const OUT_PATH = 'out/';
 
 const msigParsed = parser.parseFile('minisig.huff', modulesPath);
+const constructorParsed = parser.parseFile('constructor.huff', modulesPath)
 
 const runtimeShallow = parser.processMacro(
   'RUNTIME',
@@ -16,7 +17,17 @@ const runtimeShallow = parser.processMacro(
   msigParsed.jumptables
 ).data.bytecode;
 
+const constructorShallow = parser.processMacro(
+  'CONSTRUCTOR',
+  0,
+  [ '00' ],
+  constructorParsed.macros,
+  constructorParsed.inputMap,
+  constructorParsed.jumptables
+).data.bytecode;
+
 check(lenBytes(runtimeShallow) <= 255, "bad runtime length estimate");
+check(lenBytes(constructorShallow) <= 255, "bad constructor length estimate");
 
 const runtime = parser.processMacro(
   'RUNTIME',
@@ -27,10 +38,25 @@ const runtime = parser.processMacro(
   msigParsed.jumptables
 ).data.bytecode;
 
-check(lenBytes(runtime) === lenBytes(runtimeShallow), "bad runtime length");
+const constructor = parser.processMacro(
+  'CONSTRUCTOR',
+  0,
+  [ lenBytes(constructorShallow).toString() ],
+  constructorParsed.macros,
+  constructorParsed.inputMap,
+  constructorParsed.jumptables
+).data.bytecode;
 
+check(lenBytes(runtime) === lenBytes(runtimeShallow), "bad runtime length");
+check(lenBytes(constructor) === lenBytes(constructorShallow), "bad runtime length");
+
+const initCode = runtime + constructor
 writeBin('minisig-runtime.bin', runtime);
+writeBin('minisig-init.bin', initCode);
 console.log(`bytecode written to ${OUT_PATH}`);
+console.log(`init size        : ${lenBytes(initCode)} bytes`);
+console.log(`runtime size     : ${lenBytes(runtime)} bytes`);
+console.log(`constructor size : ${lenBytes(constructor)} bytes`);
 
 function lenBytes(str) {
   return trimBytes(str).length / 2
