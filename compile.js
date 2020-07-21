ae53f5dada475a48ff9332b5f568e363f50a3378
wolflo <wardnick@protonmail.com>
2020-07-21 13:24:34 +0400

    mv compile.js to base dir

--- compile.js
+++ compile.js
@@ -1,9 +1,9 @@
 const path = require('path');
 const fs = require('fs');
-const parser = require('../huff/src/parser');
+const parser = require('./huff/src/parser');
 
-const modulesPath = path.posix.resolve(__dirname, './huff_modules');
-const OUT_PATH = '../out/';
+const modulesPath = path.posix.resolve(__dirname, './src/huff_modules');
+const OUT_PATH = 'out/';
 
 const minisigParsed = parser.parseFile('minisig.huff', modulesPath);
 
