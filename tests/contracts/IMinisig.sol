705a77b9725dffeef7a4a57a995fca03e2e970ef
wolflo <wardnick@protonmail.com>
2021-02-04 06:35:08 +0400

    mv TargetMock.sol -> mock/

--- tests/contracts/IMinisig.sol
+++ tests/contracts/IMinisig.sol
@@ -9,11 +9,3 @@ contract IMinisig {
     function execute(address, address, uint8, uint256, uint256, bytes calldata, bytes calldata) external payable {}
     function allSigners() external view returns (address[] memory) {}
 }
-// contract IMinisig {
-//     receive() external payable {}
-//     function nonce() external view returns (uint256) {}
-//     function threshold() external view returns (uint8) {}
-//     function DOMAIN_SEPARATOR() external view returns (bytes32) {}
-//     function execute(address, uint8, uint256, uint256, bytes calldata, bytes calldata) external payable {}
-//     function allSigners() external view returns (address[] memory) {}
-// }
